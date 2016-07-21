f93ba825f6a5f580b98fbd0c8cedcf0ae10259fa
Alan Goldman <mrauxy@gmail.com>
2016-07-21 03:03:34 -0400

    Getting some pokemon back!

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,9 @@ from pgoapi import PGoApi
 from pgoapi.utilities import f2i, h2f
 import yaml
 import os
+import time
 import json
+import random
 from s2sphere import Cell, CellId, LatLng
 from google.protobuf.internal import encoder
 
@@ -35,29 +37,76 @@ def get_cell_ids(lat, long, radius = 10):
 
     # Return everything
     return sorted(walk)
+  
 
+def generate_spiral(starting_lat, starting_lng, step_size, step_limit):
+    coords = [{'lat': starting_lat, 'lng': starting_lng}]
+    steps,x,y,d,m = 1, 0, 0, 1, 1
+    rlow = 0.0
+    rhigh = 0.0005
 
-def find_pokemon(client, lat, long):
-    client.set_position(lat, long, 0)
-    cell_ids = get_cell_ids(lat, long)
-    timestamps = [0,] * len(cell_ids)
-    client.get_map_objects(latitude=f2i(lat), longitude=f2i(long), since_timestamp_ms=timestamps, cell_id=cell_ids)
+    while steps < step_limit:
+        while 2 * x * d < m and steps < step_limit:
+            x = x + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
+        while 2 * y * d < m and steps < step_limit:
+            y = y + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
 
-    response = client.call()
-    print(response)
+        d = -1 * d
+        m = m + 1
+    return coords
+
     
+def format_pokemon(pokemon):
+    print(pokemon)
+    return {
+        'id': pokemon['pokemon_data']['pokemon_id'],
+        'lat': pokemon['latitude'],
+        'long': pokemon['longitude'],
+        'time_till_hidden': pokemon['time_till_hidden_ms']/1000
+    }
+  
+def find_pokemon(client, lat, long):
+    step_size = 0.0015
+    step_limit = 49
+    coords = generate_spiral(lat, long, step_size, step_limit)
+    pokemons = []
+    
+    for coord in coords:
+        lat = coord['lat']
+        lpng = coord['lng']
+        client.set_position(lat, long, 0)
+        
+        cell_ids = get_cell_ids(lat, long)
+        timestamps = [0,] * len(cell_ids)
+        client.get_map_objects(latitude=f2i(lat), longitude=f2i(long), since_timestamp_ms=timestamps, cell_id=cell_ids)
+        
+        response = client.call()
+        if response['responses']['GET_MAP_OBJECTS']['status'] == 1:
+            for map_cell in response['responses']['GET_MAP_OBJECTS']['map_cells']:
+                if 'wild_pokemons' in map_cell:
+                    for pokemon in map_cell['wild_pokemons']:
+                        print(pokemon)
+                        pokemons.append(pokemon)
+    
+    return list(map(format_pokemon, pokemons))
+                    
 if __name__ == '__main__':
     config = load_config()
+    pokemon_db = load_pokemon()
     client = PGoApi()
     
     logged_in = client.login(config['auth_service'], config['username'], config['password'])
     if not logged_in:
         print("Could not login")
         exit()
-    
-    #client.get_player()
-    #response = client.call()
-    #print(response)
-    #print(json.dumps(response))
-    
-    find_pokemon(client, config['lat'], config['long'])
+
+    pokemon = find_pokemon(client, config['lat'], config['long'])
+    print(pokemon)
